7f1d9f2cd15f2f8eacd4b3934e761b400a18b5c6
Mattias Kallman <33825140+kallmanm@users.noreply.github.com>
2019-02-15 10:07:06 +0200

    minor updates
    added more content

--- src/Node.java
+++ src/Node.java
@@ -7,6 +7,9 @@ public class Node {
     protected double latitude;
     protected double longitude;
     protected List<Node> neighbours = new ArrayList<>();
+    protected Node startVertex;
+    protected Node destination;
+    protected Node currentVertex;
 
     //---- Constructor ----
     public Node(String name, double latitude, double longitude){
@@ -26,10 +29,19 @@ public class Node {
     public void setLongitude(double longitude){
         this.longitude =longitude;
     }
+    public void setStartVertex(Node startVertex){
+        this.startVertex = startVertex;
+    }
+    public void setDestination(Node destination){
+        this.destination = destination;
+    }
+    public void setCurrentVertex(Node currentVertex){
+        this.currentVertex = currentVertex;
+    }
 
     //---- Add Functions ----
     public void addNeighbour(Node neighbour){
-        //todo:fix this so can add many neighbours
+
         this.neighbours.add(neighbour);
     }
 
@@ -46,30 +58,66 @@ public class Node {
     public List<Node> getNeighbours(){
         return neighbours;
     }
-
-
-    //---- Methods & Functions ----
-
-
-    public static void printNodes(List<Node>nodes){
-        for(Node node:nodes){
-            System.out.println("------------------------");
-            System.out.println("City Name: "+node.getName());
-            System.out.println("City Neighbours: ");
-            List <Node> nodeNeighbours = new ArrayList<>();
-            nodeNeighbours = node.getNeighbours();
-
-            for(Node neighbour:nodeNeighbours) System.out.println("     "+neighbour.getName());
-
-            System.out.println("City Coordinates: ");
-            System.out.println("     Latitude: "+node.getLatitude());
-            System.out.println("     Longitude: "+node.getLongitude());
-        }
+    public Node getStartVertex(){
+        return startVertex;
+    }
+    public Node getDestination(){
+        return destination;
+    }
+    public Node getCurrentVertex(){
+        return currentVertex;
     }
 
+    //---- Other Methods & Functions ----
 
+    //Returns the distance between two locations based on their latitude and longitude coordinates
+    public double getDistance(double lon1, double lat1, double lon2, double lat2){
+        lon1 = lon1*Math.PI/180.0;
+        lat1 = lat1*Math.PI/180.0;
+        lon2 = lon2*Math.PI/180.0;
+        lat2 = lat2*Math.PI/180.0;
+
+
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = Math.pow(Math.sin(dlat/2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon/2), 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double km = 6367 * c;
+
+        return km;
+    }
+
+    //returns H-value of A* algorithm
+    //todo: optimize calculateH
+    public double calculateH(Node currentVertex,Node destination){
+
+        return getDistance(currentVertex.getLongitude(),currentVertex.getLatitude(),destination.getLongitude(),destination.getLatitude());
+    }
+
+    //returns G-value of A* algorithm
+    //todo: optimize calculateG
+    public double calculateG(Node startVertex){
+
+        //calculate from start all distance traveled via different nodes
+        return getDistance(startVertex.getLongitude(),startVertex.getLatitude(),this.getLongitude(),this.getLatitude());
+    }
+
+    //returns F-value of A* algorithm
+    //todo: optimzie calculateF
+    public double calculateF(Node startVertex,Node destination, Node currentVertex){
+        //F = G + H
+        double f = calculateG(startVertex) + calculateH(currentVertex,destination);
+        return f;
+    }
+
+    //Todo: create A* algorithm function
+    public void calculateAStar(){
+        //todo:make code
+    }
+
+    //Creates the graph data used in this assignment
     public static ArrayList<Node> createGraph(){
-        //todo:fix this method so it works as intended...
+
         Node hki = new Node("Helsingfors", 60.1640504, 24.7600896);
         Node tpe = new Node("Tammerfors", 61.6277369, 23.5501169);
         Node tku = new Node("Abo", 60.4327477, 22.0853171);
@@ -107,5 +155,20 @@ public class Node {
         graph.add(lhi);
 
         return graph;
+    }
+
+    //Prints out in console all Nodes and all relating information to the Nodes
+    public static void printNodes(List<Node>nodes){
+        for(Node node:nodes){
+            System.out.println("-----------------------------");
+            System.out.println("City Name: "+node.getName());
+            System.out.println("City Neighbours: ");
+
+            for(Node neighbour:node.getNeighbours()) System.out.println("     "+neighbour.getName());
+
+            System.out.println("City Coordinates: ");
+            System.out.println("     Latitude: "+node.getLatitude());
+            System.out.println("     Longitude: "+node.getLongitude());
+        }
     }
 }
--- src/Start.java
+++ src/Start.java
@@ -1,14 +1,21 @@
 
+import java.text.*;
 import java.util.*;
 
 public class Start {
 
-    public static void main(String []args){
+    public static void main(String [] args){
 
+        //Trims doubles to give more human readable format.
+        DecimalFormat df = new DecimalFormat("#.##");
 
-        List<Node> nodes = new ArrayList(Node.createGraph());
-
+        //Loads in data nodes.
+        List <Node> nodes = new ArrayList(Node.createGraph());
 
         Node.printNodes(nodes);
+
+        double test = nodes.get(0).calculateH(nodes.get(0),nodes.get(3));
+        System.out.println("Distance between "+nodes.get(0).getName()+" and " + nodes.get(3).getName()+" is: "+df.format(test)+" km.");
+
     }
 }
